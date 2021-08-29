dd7fc701a22fbf3a86e1ef8e50ab33d9516b7a8f
gauravbhatt13 <gauravbhatt13@gmail.com>
2021-08-30 00:07:14 +1000

    removed serverless

--- lambda.js
+++
@@ -1,4 +0,0 @@
-const serverlessExpress = require("@vendia/serverless-express");
-const app = require("./dist/preparepte/serverless/main");
-const serverProxy = serverlessExpress.createServer(app.server);
-module.exports.handler = (event, context) => serverlessExpress.proxy(serverProxy, event, context);
